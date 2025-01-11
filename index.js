eb659de1b10cfab78ad95a4042481c5152eff84a
ShubhamVG <vgshubham49@gmail.com>
2025-01-11 17:49:21 +0530

    Fix the d sound by changing invalid path
    Previous path was sounds/tom-4.mp4 but there is no sounds folder

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function makeSound(key) {
             break;
 
         case "d":
-            var tom4 = new Audio('sounds/tom-4.mp3');
+            var tom4 = new Audio('tom-4.mp3');
             tom4.play();
             break;
 
